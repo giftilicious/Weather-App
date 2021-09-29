20179334cb596cf8cbaee694ad118cc33b3b5d3d
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-09-28 22:25:17 -0400

    Added function for user input in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,106 @@
-let userInput = document.querySelector('.form-control');
+// let cityName = document.querySelector('.form-control');
+let searchBtnEl = document.querySelector('#search-btn');
 let prevSearchEl = document.querySelector('.prev-search');
 let currentWeatherEl = document.querySelector('#current-weather');
 let forecastEl = document.querySelector('#forecast-info');
+let weatherAPIKey = '0dec5f9d90212222e001cf9eff9b7150';
 
-// Fetch weather details
 
-function getAPI() {
-    requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=0dec5f9d90212222e001cf9eff9b7150';
 
-    fetch(requestUrl)
+// let weatherData = document.getElementById("weather_data");
+
+searchBtnEl.addEventListener('click', function (event) {
+    event.preventDefault();
+    let cityName = document.querySelector('.form-control').value;
+    requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "=&appid=" + weatherAPIKey;
+    if (cityName == "") {
+        alert("Enter a city name");
+    } else {
+        fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            for (let i = 0; i < data.length; i++) {
-                let listItem = document.createElement('li');
-                currentWeatherEl.textContent = data[i];
-                console.log(data);
-            }            
-        });
-}
+            console.log(data);
+            // const temperature = document.createElement("p");
+            // temperature.textContent = "Temperature: " + data.main.feels_like;
+            // currentWeatherEl.appendChild(temperature) 
 
-getAPI();
+            // const humidity = document.createElement("p");
+            // humidity.textContent = "Humidity: " + data.main.humidity;
+            // currentWeatherEl.appendChild(humidity)
 
-    // renderInfo(city, data);
+            // const humidity = document.createElement("p");
+            // humidity.textContent = "Humidity: " + data.main.humidity;
+            // currentWeatherEl.appendChild(humidity)  
+            });
+    }
+});
+
+/* <form>
+ <input type="text" id="cityName" placeholder="Enter a city name"><br>
+ <input type="submit" value="Get Weather Information" id="weather_data">
+</form>
+
+<div id="display_data"></div> */
+
+
+
+
+
+// Submit button
+// function showWeather(event) {
+//     // Prevent default action
+//     event.preventDefault();
+//     console.log(event);
+//     let result = userInput.value;
+//     currentWeatherEl.textContent = result;
+// }
+
+// searchBtnEl.addEventListener("click", showWeather);
+
+// Fetch weather details
+// function getCurrentAPI() {
+//     let city = userInput;
+//     requestUrl = "https://api.openweathermap.org/data/2.5/weather?q" + city + "=&appid=" + weatherAPIKey;
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data)
+            // const temperature = document.createElement("p");
+            // temperature.textContent = "Temperature: " + data.main.feels_like;
+            // currentWeatherEl.appendChild(temperature) 
+
+            // const humidity = document.createElement("p");
+            // humidity.textContent = "Humidity: " + data.main.humidity;
+            // currentWeatherEl.appendChild(humidity)
+
+            // const humidity = document.createElement("p");
+            // humidity.textContent = "Humidity: " + data.main.humidity;
+            // currentWeatherEl.appendChild(humidity)       
+//         });
+// }
+
+// getCurrentAPI();
+
+ // Weather info:
+ // Temperature
+ // Wind
+ // Humidity
+ // UV Index (with colour alert)
+
+
+ // Fetch 5-day forecast details
+//  function getForecastAPI() {
+
+//  }
+
+
+// User input
+
+
 
 
 
