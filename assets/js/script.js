fa386cc47bbed78aff99101ce3ad8a4fdd23e52d
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-09-28 23:26:24 -0400

    Added forecast elements to HTML and updated js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 let searchBtnEl = document.querySelector('#search-btn');
 let prevSearchEl = document.querySelector('.prev-search');
 let currentWeatherEl = document.querySelector('#current-weather');
-let forecastEl = document.querySelector('#forecast-info');
+let futureForecast = document.querySelector('.card');
+// let forecastEl = document.querySelector('#forecast-info');
 let weatherAPIKey = '0dec5f9d90212222e001cf9eff9b7150';
 
 
@@ -22,31 +23,39 @@ searchBtnEl.addEventListener('click', function (event) {
         })
         .then(function (data) {
             console.log(data);
-            // const temperature = document.createElement("p");
-            // temperature.textContent = "Temperature: " + data.main.feels_like;
-            // currentWeatherEl.appendChild(temperature) 
+            const temperature = document.createElement("p");
+            temperature.textContent = "Temperature: " + data.main.feels_like;
+            currentWeatherEl.appendChild(temperature) 
+            futureForecast.appendChild(temperature) 
+
+            // const wind = document.createElement("p");
+            // wind.textContent = "Wind: " + data.main.humidity;
+            // currentWeatherEl.appendChild(wind)
+            // futureForecast.appendChild(wind)
 
             // const humidity = document.createElement("p");
             // humidity.textContent = "Humidity: " + data.main.humidity;
             // currentWeatherEl.appendChild(humidity)
+            // futureForecast.appendChild(humidity)  
 
-            // const humidity = document.createElement("p");
-            // humidity.textContent = "Humidity: " + data.main.humidity;
-            // currentWeatherEl.appendChild(humidity)  
-            });
+            // const uvIndex = document.createElement("p");
+            // uvIndex.textContent = "UV Index: " + data.main.humidity;
+            // currentWeatherEl.appendChild(uvIndex) 
+            // futureForecast.appendChild(uvIndex) 
+            })           
     }
 });
 
-/* <form>
- <input type="text" id="cityName" placeholder="Enter a city name"><br>
- <input type="submit" value="Get Weather Information" id="weather_data">
-</form>
-
-<div id="display_data"></div> */
-
-
+// Store weather info
+//   $('.btn').on('click', function (event) {
+//     let text = $(this).parent().siblings('.input-group').children('.form-control').val()
+//     localStorage.setItem('Task', text);
+//     event.preventDefault();
+// });  
 
 
+
+//////////////////////////////////////////////////////////
 
 // Submit button
 // function showWeather(event) {
@@ -104,9 +113,3 @@ searchBtnEl.addEventListener('click', function (event) {
 
 
 
-// Store weather info
-//   $('.btn').on('click', function (event) {
-//     let text = $(this).parent().siblings('.input-group').children('.form-control').val()
-//     localStorage.setItem('Task', text);
-//     event.preventDefault();
-// });  
