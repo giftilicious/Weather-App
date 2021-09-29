2673230d6f6cc91ceac209f19b13b612712e782c
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-09-29 10:59:09 -0400

    Added weather icon to data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // let cityName = document.querySelector('.form-control');
 let searchBtnEl = document.querySelector('#search-btn');
-let prevSearchEl = document.querySelector('.prev-search');
+// let prevSearchEl = document.querySelector('.prev-search');
+// let prevSearchEl = document.querySelector('#city-list');
 let currentWeatherEl = document.querySelector('#current-weather');
 let futureForecast = document.querySelector('.card');
+let oldResultsEl = document.querySelector('#oldResults');
 // let forecastEl = document.querySelector('#forecast-info');
 let weatherAPIKey = '0dec5f9d90212222e001cf9eff9b7150';
 
@@ -23,9 +25,11 @@ searchBtnEl.addEventListener('click', function (event) {
         })
         .then(function (data) {
             console.log(data);
-            let city = document.createElement('h3');
-            city.textContent = data.name + data.weather.icon;
-            currentWeatherEl.appendChild(city);
+            let location = document.createElement('h3');
+            let weatherUrl = data.weather[0].icon;
+            let icon = `https://openweathermap.org/img/w/${weatherUrl}.png`
+            location.innerHTML = `${data.name} <img src=${icon}>`;
+            currentWeatherEl.appendChild(location);
             // futureForecast.appendChild(city);
             
             let temperature = document.createElement("p");
@@ -47,20 +51,52 @@ searchBtnEl.addEventListener('click', function (event) {
             uvIndex.textContent = "UV Index: " + data.main.humidity;
             currentWeatherEl.appendChild(uvIndex) 
             // futureForecast.appendChild(uvIndex) 
-            })           
+            })   
+        .then(function (data) {
+            let weatherUrl = data.weather[0].icon;
+            let icon = `https://openweathermap.org/img/w/${weatherUrl}.png`
+            location.innerHTML = `${data.name} <img src=${icon}>`;
+
+            let temperature = document.createElement("p");
+            temperature.textContent = "Temperature: " + data.main.feels_like + "°C";
+            futureForecast.appendChild(temperature); 
+        })        
     }
 });
 
-// Store weather info
-//   $('.btn').on('click', function (event) {
-//     let text = $(this).parent().siblings('.input-group').children('.form-control').val()
-//     localStorage.setItem('Task', text);
-//     event.preventDefault();
-// });  
 
 
+// let cityList = [];
+
+// function renderCity() {
+//     // Clear todoList element and update todoCountSpan
+//     // prevSearchEl.innerHTML = "";
+//     // oldResultsEl.textContent = cityList.length;
+  
+//     // Render a new li for each todo
+//     for (let i = 0; i < cityList.length; i++) {
+//       let city = cityList[i];
+  
+//       let li = document.createElement("li");
+//       li.textContent = city;
+//       li.setAttribute("data-index", i);
+  
+//     //   let button = document.createElement("button");
+//     //   button.textContent = "Complete ✔️";
+  
+//     //   li.appendChild(button);
+//     prevSearchEl.appendChild(li);
+//     }
+//   }
 
 //////////////////////////////////////////////////////////
+
+// Store weather info
+//   $('#search-btn').on('click', function (event) {
+//     let text = $(this).parent().siblings('.card').children('.city-list').val()
+//     // localStorage.setItem('Task', text);
+//     event.preventDefault();
+// });  
 
 // Submit button
 // function showWeather(event) {
